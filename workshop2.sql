4c72cbbdb2859077566a63d408cd4ea7e3da2fab
MDEPSI <maxime.dupont49280@gmail.com>
2018-04-11 09:16:41 +0200

    update bdd

--- workshop2.sql
+++ workshop2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 10 Avril 2018 à 11:22
+-- Généré le :  Mer 11 Avril 2018 à 09:15
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `workshop2`
 --
-CREATE DATABASE IF NOT EXISTS `workshop2` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `workshop2`;
 
 -- --------------------------------------------------------
 
@@ -37,7 +35,19 @@ CREATE TABLE IF NOT EXISTS `apprenant` (
   `PASSWORD` char(32) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `FK_APPRENANT_EQUIPE` (`ID_EQUIPE`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Contenu de la table `apprenant`
+--
+
+INSERT INTO `apprenant` (`ID`, `ID_EQUIPE`, `NOM`, `PRENOM`, `LOGIN`, `PASSWORD`) VALUES
+(2, NULL, 'DUPONT', 'Maxime', 'login', 'password'),
+(10, 5, '$nom10', '$prenom', '$login', '$password'),
+(11, 5, '$nom', '$prenom11', '$login', '$password'),
+(12, 6, '$nom', '$prenom', '$login', '$password'),
+(13, 5, '$nom', '$prenom', '$login', '$password'),
+(14, 5, '$nom', '$prenom', '$login', '$password');
 
 -- --------------------------------------------------------
 
@@ -52,7 +62,17 @@ CREATE TABLE IF NOT EXISTS `equipe` (
   `NBJETONS` bigint(4) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `FK_EQUIPE_PROJET` (`ID_PROJET`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Contenu de la table `equipe`
+--
+
+INSERT INTO `equipe` (`ID`, `ID_PROJET`, `LIBELLE`, `NBJETONS`) VALUES
+(5, 1, '$libelle5', 5),
+(6, 1, '$libelle', 0),
+(7, 1, '$libelle', 0),
+(8, 1, '$libelle', 20);
 
 -- --------------------------------------------------------
 
@@ -67,8 +87,26 @@ CREATE TABLE IF NOT EXISTS `intervenant` (
   `LOGIN` char(32) DEFAULT NULL,
   `PASSWORD` char(32) DEFAULT NULL,
   `STATUS` char(32) DEFAULT NULL,
+  `SPECIALITE` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Contenu de la table `intervenant`
+--
+
+INSERT INTO `intervenant` (`ID`, `NOM`, `PRENOM`, `LOGIN`, `PASSWORD`, `STATUS`, `SPECIALITE`) VALUES
+(1, 'NOM', 'PRENOM', 'login', 'password', 'ok', NULL),
+(2, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(3, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(4, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(5, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(6, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(7, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(8, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(9, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(10, '$nom', '$prenom', '$login', '$password', '$status', NULL),
+(11, '$nom', '$prenom', '$login', '$password', '$status', NULL);
 
 -- --------------------------------------------------------
 
@@ -83,7 +121,24 @@ CREATE TABLE IF NOT EXISTS `projet` (
   `NBJETONS` bigint(4) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `FK_PROJET_RESPONSABLE` (`ID_RESPONSABLE`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Contenu de la table `projet`
+--
+
+INSERT INTO `projet` (`ID`, `ID_RESPONSABLE`, `LIBELLE`, `NBJETONS`) VALUES
+(1, 1, '$libelle1', 20),
+(2, 1, '$libelle', 20),
+(3, 1, '$libelle', 20),
+(4, 1, '$libelle', 20),
+(5, 1, '$libelle', 20),
+(6, 1, '$libelle', 20),
+(7, 1, '$libelle', 20),
+(8, 1, '$libelle', 20),
+(9, 1, '$libelle', 20),
+(10, 1, '$libelle', 20),
+(11, 1, '$libelle', 20);
 
 -- --------------------------------------------------------
 
@@ -98,6 +153,13 @@ CREATE TABLE IF NOT EXISTS `projet_intervenant` (
   KEY `FK_PROJET_INTERVENANT_INTERVENANT` (`ID_1`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `projet_intervenant`
+--
+
+INSERT INTO `projet_intervenant` (`ID`, `ID_1`) VALUES
+(1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -111,7 +173,14 @@ CREATE TABLE IF NOT EXISTS `responsable` (
   `LOGIN` char(32) DEFAULT NULL,
   `PASSWORD` char(32) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `responsable`
+--
+
+INSERT INTO `responsable` (`ID`, `NOM`, `PRENOM`, `LOGIN`, `PASSWORD`) VALUES
+(1, 'RESP', 'PEDAGO', 'login', 'pass');
 
 --
 -- Contraintes pour les tables exportées
